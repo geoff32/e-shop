45ba68208f9b1271649837ebb77eee45f3214e1a
HERB Geoffroy <geoffroy.herb@hotmail.fr>
2025-10-03 21:29:08 +0200

    close cart details when switch page

--- src/features/cart/cart.tsx
+++ src/features/cart/cart.tsx
@@ -1,13 +1,19 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { faBasketShopping } from "@fortawesome/free-solid-svg-icons";
 import { IconWithCounter, Offcanvas, OffcanvasBody, OffcanvasHeader, OffcanvasTitle } from "../../components";
 import CartDetails from "./cartDetails";
 import { useAppSelector } from "../../app/hooks";
 import { selectCart } from "./cartSlice";
+import { useLocation } from "react-router-dom";
 
 const Cart = () => {
   const [show, setShow] = useState(false);
-    const cart = useAppSelector(selectCart);
+  const cart = useAppSelector(selectCart);
+  const location = useLocation();
+
+  useEffect(() => {
+    setShow(false);
+  }, [location]);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
